a3993dcdb524ffa6df49a8cad648d6882eea7c51
EC2 Default User <ec2-user@ip-172-31-6-80.ec2.internal>
2021-05-06 06:49:58 +0000

    [Add]Book検索機能

--- app/controllers/searchs_controller.rb
+++ app/controllers/searchs_controller.rb
@@ -2,11 +2,11 @@ class SearchsController < ApplicationController
   def search
     @target = params[:target]
     option = params[:option]
-    search = params[:search]
+    @search = params[:search]
     if @target == "1"
-      @result = User.search(option, search)
+      @result = User.search(option, @search)
     elsif @target == "2"
-      @result = Book.search(option, search)
+      @result = Book.search(option, @search)
     else
       @result = "検索対象が設定されていません"
     end
--- app/models/book.rb
+++ app/models/book.rb
@@ -7,6 +7,21 @@ class Book < ApplicationRecord
     favorites.where(user_id: user.id).exists?
   end
 
+  def self.search(option, search)
+    if option == "perfect_match"
+      Book.where("title LIKE?", "#{search}")
+    elsif option == "forward_match"
+      Book.where("title LIKE?", "#{search}%")
+    elsif option == "backward_match"
+      Book.where("title LIKE?", "%#{search}")
+    elsif option == "partial_match"
+      Book.where("title LIKE?", "%#{search}%")
+    else
+      Book.all
+    end
+  end
+
+
   validates :title, presence: true
   validates :body, presence: true, length: {maximum: 200}
 
